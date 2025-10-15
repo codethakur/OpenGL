2ad1018b87120810b3a8c9b71f6a906c86a47163
codethakur <kishankt2017@gmail.com>
2025-10-15 13:19:19 +0530

    Add SIMD and core trigonometric functions to GLM
    - Introduced SIMD support for trigonometric functions in `trigonometric.h` and `vector_relational.h`.
    - Implemented core trigonometric functions in `trigonometric.hpp`, including radians, degrees, sin, cos, tan, and their inverse functions.
    - Added vector types for 2D, 3D, and 4D vectors in `vec2.hpp`, `vec3.hpp`, and `vec4.hpp`.
    - Enhanced vector relational operations in `vector_relational.hpp` to support component-wise comparisons and logical operations.

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,3 +1,6 @@
+#define IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD   
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <string>
@@ -9,7 +12,13 @@
 #include "VertexBufferLayout.hpp"
 #include "Shader.hpp"
 #include "Texture.hpp"
-#include"vendor/glm/gtc/matrix_transform.hpp"
+#include "vendor/glm/gtc/matrix_transform.hpp"
+#include "BackgroundRenderer.hpp"
+#include "vendor/imgui/imgui.h"
+#include "vendor/imgui/imgui_impl_glfw.h"
+#include "vendor/imgui/imgui_impl_opengl3.h"
+
+
 
 int main(void)
 {
@@ -46,17 +55,17 @@ int main(void)
     // ----------------- Vertex Data -----------------
     float positions[] = {
         //   X    Y      U       V
-        -0.5f, -0.5f,   0.0f,   0.0f, // 0
-         0.5f, -0.5f,   1.0f,   0.0f, // 1
-         0.5f,  0.5f,   1.0f,   1.0f, // 2
-        -0.5f,  0.5f,   0.0f,   1.0f  // 3
+        -0.5f, -0.5f, 0.0f, 0.0f, // 0
+        0.5f, -0.5f, 1.0f, 0.0f,  // 1
+        0.5f, 0.5f, 1.0f, 1.0f,   // 2
+        -0.5f, 0.5f, 0.0f, 1.0f   // 3
     };
 
     unsigned int indices[] = {
         0, 1, 2,
         2, 3, 0};
-    // GLCall(glEnable(GL_BLEND));
-    // GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
     VertexArray va;
     VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
@@ -68,73 +77,129 @@ int main(void)
     IndexBuffer ib(indices, 6);
 
     // ----------------- Matrix -----------------
-    glm::mat4 proj = glm::ortho(-3.0f, 3.0f, -2.0f, 2.0f, -1.0f, 1.0f);
+    glm::mat4 proj = glm::ortho(-3.0f, 3.0f, -2.0f, 2.0f, -1.0f, 1.0f); // For Projection view
 
-    //glm::mat4 proj  = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
-    glm::mat4 view  = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, .0f));//camera view
-    glm::mat4 model = glm::mat4(1.0f);    //rotate a cube, move it left/right, scale it bigger/smaller.
-    glm::mat4 mvp = proj * view * model; // proj * view * model=> order
-    // ----------------- Shader -----------------
+    // // ----------------- Shader -----------------
     Shader shader("res/shaders/Basic.shader");
-    shader.setUniformMat4f("u_MVP", proj);
     shader.Bind();
-    // int loc = getUniformLocation("u_MVP");
-    // std::cout << "u_MVP location = " << loc << std::endl;
 
     // ----------------- Texture -----------------
     Texture texture("res/textures/codethakur.png");
     texture.Bind(0);                     // bind to slot 0
     shader.setUniform1i("u_Texture", 0); // tell shader to use slot 0
-    shader.setUniformMat4f("u_MVP",  proj);
+    shader.setUniformMat4f("u_MVP", proj);
 
     // ----------------- Renderer -----------------
     Renderer renderer;
     float r = 0.0f;
     float increment = 0.05f;
+    float angle = 0.0f;
+    float f = 0.0f;
+    float moveX = 0.0f;   // left-right
+    float moveY = 0.0f;   // up-down
+
+     // ----------------- ImGui Setup -----------------
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // ----------------- App State -----------------
+    bool show_demo_window = false;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.71f, 0.74f, 0.76f, 1.00f);
+
 
     // ----------------- Loop -----------------
-    float angle = 0.0f;
-    while (!glfwWindowShouldClose(window))
+
+    
+       while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.235f, 0.235f, 0.235f, 0.05f);
+        glfwPollEvents();
+
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // ImGui Window
+        {
+            ImGui::Begin("Hello, from ImGui");
+            ImGui::Checkbox("Demo Window", &show_demo_window);
+            ImGui::Checkbox("Another Window", &show_another_window);
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            angle += f; 
+            ImGui::SliderFloat("Move X (Left-Right)", &moveX, -2.5f, 2.5f);
+            ImGui::SliderFloat("Move Y (Up-Down)", &moveY, -1.5f, 1.5f);
+
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            
+            if (ImGui::Button("Reset Button"))
+            {
+                moveX = 0.0f;
+                moveY = 0.0f;
+                f = 0.0f;
+            }
+
+                   
+            ImGui::End();
+
+            if (show_another_window)
+            {
+                ImGui::Begin("Another Window", &show_another_window); 
+                ImGui::Text("Hello from another window!");
+                
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                            1000.0f / io.Framerate, io.Framerate);
+                
+               
+
+                ImGui::End();
+            }
+
+        }
+
+        // Clear background
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         renderer.Clear();
 
+        // Animate rotation and color
         shader.Bind();
         texture.Bind(0);
 
-        // animate color tint
         shader.setUniform4f("u_Color", 1.0f, r, 0.2f, 1.0f);
-        // ----------------- View (camera, dynamic)Basically-> Move=>Left/Right + Top/Bottom -----------------
-        float t = (float)glfwGetTime();
-        glm::mat4 view = glm::translate(glm::mat4(1.0f),
-                                        glm::vec3(-sin(t) , cos(t), 0.0f));
+        glm::mat4 view  = glm::translate(glm::mat4(1.0f), glm::vec3(moveX, moveY, 0.0f));
+        glm::mat4 model = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
+        //glm::mat4 model = glm::mat4(1.0f);
 
-        // ----------------- Model (object, dynamic) -----------------
-        angle += 0.02f;
-        glm::mat4 model = glm::rotate(glm::mat4(1.0f), angle,  glm::vec3(0.5f, 0.5f, 1.0f));
-
-        // ----------------- Final MVP -----------------
         glm::mat4 mvp = proj * view * model;
         shader.setUniformMat4f("u_MVP", mvp);
-    
+
         renderer.Draw(va, ib, shader);
 
-
-        // animate "r"
+        // Animate color oscillation
         if (r > 1.0f)
             increment = -0.05f;
-
         else if (r < 0.0f)
             increment = 0.05f;
         r += increment;
-       // angle += increment;
+
+        // Render ImGui over everything
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
 
+    // ----------------- Cleanup -----------------
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
-
-
